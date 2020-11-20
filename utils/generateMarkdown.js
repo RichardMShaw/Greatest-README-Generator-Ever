77bf5f0f186022cdcc1aa812b74d7a17cd6315fa
Richard Shaw <richardmorganshaw@gmail.com>
2020-11-19 21:50:02 -0800

    updated code and testing out readme on github

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,33 +17,32 @@ const gl = require('./generateLicense')
 function generateMarkdown(data) {
   let license = gl.getLicense(data.license)
   let readme = `
-    ${license.badge}
-    # ${data.title}
-    ## Description
-    ${data.description}
-    ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    ## Installation
-    ${data.installation}
-    ## Usage
-    ${data.usage}
-    ## License
-    ${license.badge}
-    ## Contributing
-    ${data.contributing}
-    ## Tests
-    ${data.tests}
-    ## Questions
-    ${data.questions}
-    Github Profile: https://github.com/${data.username}
-    Email: ${data.email}
-  `
-  readme = readme.replace('  ', '')
+${license.badge}
+# ${data.title}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${license.badge}
+## Contributing
+${data.contributing}
+## Tests
+${data.tests}
+## Questions
+${data.questions}
+Github Profile: https://github.com/${data.username}
+Email: ${data.email}
+`
   return readme
 }
 
